5756f11548cf05e06fe6b77e83a10f283a929fec
Bhawesh Ranjan Mahto <bhaweshranjan143@gmail.com>
2024-10-01 18:37:53 +0530

    removed

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const errorHandler = require('./src/middleware/errorHandler');
 
 const { Client, LocalAuth,MessageMedia } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
-const multer = require('multer');
 
 
 const app = express();
@@ -42,27 +41,7 @@ client.on('ready', () => {
 
 client.initialize();
 
-// API endpoints
-app.post('/send-message', async (req, res) => {
-  const { number, messageType, message } = req.body;
 
-  try {
-    const chatId = number.includes('@c.us') ? number : `${number}@c.us`;
-
-    if (messageType === 'text') {
-      await client.sendMessage(chatId, message);
-    } else if (messageType === 'media') {
-      const media = await MessageMedia.fromUrl(message); // Use MessageMedia.fromUrl for URLs
-      await client.sendMessage(chatId, media);
-    } else {
-      throw new Error('Unsupported message type');
-    }
-
-    res.status(200).json({ status: 'success', message: 'Message sent' });
-  } catch (error) {
-    res.status(500).json({ status: 'error', message: error.message });
-  }
-});
 
 
 
